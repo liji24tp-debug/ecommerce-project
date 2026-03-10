fb73fcc8c1aebf42e306f6e74cf7c2359aad393a
liji24tp-debug <liji24tp@student.ju.se>
2026-03-10 10:59:09 +0100

    create basic web

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Add products to localStorage
+function addToCart(productName) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  cart.push(productName);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert(productName + " added to cart!");
+}
